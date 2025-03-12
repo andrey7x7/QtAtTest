0e6bb299e9f511720d90195e761f38bf5d8b4b20
52velichkoas <velichkoas@utgard.cbr.ru>
2025-03-12 10:44:57 +0600

    слежение за обектом

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,14 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowTitle("AiTest");
     connect(ui->start, SIGNAL(triggered()), this, SLOT(showMessage()));
     connect(ui->exit, SIGNAL(triggered()), this, SLOT(close()));
+
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,782,533);
+    ui->graphicsView->setScene(scene);
+    widthPlane = 782;
+    heightPlane = 533;
+
+    generateField();
 }
 
 MainWindow::~MainWindow()
@@ -16,8 +24,58 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+/**
+    Коммент
+*/
 void MainWindow::showMessage()
 {
+    model = new Model();
+    scene->addItem(model);
+    model->setPos(ui->graphicsView->width()/2, ui->graphicsView->height()/2);
     ui->statusbar->showMessage("start");
+    pos = model->pos();
 }
 
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+        case Qt::Key_Up:
+            pos.y()>0 ? pos.setY(pos.y()-10) : pos.setY(pos.y());
+            break;
+        case Qt::Key_Down:
+            pos.setY(pos.y()+10);
+            break;
+        case Qt::Key_Left:
+            pos.x()>0 ? pos.setX(pos.x()-10) : pos.setX(pos.x());
+            break;
+        case Qt::Key_Right:
+            pos.setX(pos.x()+10);
+            break;
+    }
+
+    model->setPos(pos);
+    scene->setSceneRect(pos.x()-widthPlane/2, pos.y()-heightPlane/2, widthPlane-5, heightPlane-7);
+
+    //смещение области видимости
+//    if(pos.x()>widthPlane/2 || pos.y()>heightPlane/2){
+//        QRectF rect;
+//        rect.setWidth(widthPlane-5);
+//        rect.setHeight(heightPlane-7);
+//        rect.setX(pos.x()>widthPlane/2 ? pos.x()-widthPlane/2 : widthPlane/2);
+//        rect.setY(pos.y()>heightPlane/2 ? pos.y()-heightPlane/2 : heightPlane/2);
+//        scene->setSceneRect(rect);
+//        //scene->setSceneRect(pos.x()-widthPlane/2, pos.y()-heightPlane/2, widthPlane-5, heightPlane-7);
+//    }
+    qDebug()<<pos;
+}
+
+void MainWindow::generateField()
+{
+    for(int w=0; w<1501 ; w+=50){
+        scene->addLine(w,0,w,1500);
+    }
+    for(int h=0; h<1501 ; h+=50){
+        scene->addLine(0,h,1500,h);
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QDebug>
+#include <QKeyEvent>
+#include <QException>
+
+#include "model.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,8 +23,18 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    Model *model;
+    int widthPlane;
+    int heightPlane;
+    QPointF pos;
 
 private slots:
     void showMessage();
+    void generateField();
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
 };
 #endif // MAINWINDOW_H
